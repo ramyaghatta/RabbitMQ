d453ffc7c1df2961c5d72e4cb3e4187accf3ab5a
Ramya Ghatta <ramyaghatta@gmail.com>
2020-10-10 02:21:29 +0530

    RabbitMQ C# apllication

---
+++ Send/Send.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+namespace Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+             var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "Queue1",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            //string message = "Hello World!Rabbit MQ Message";
+            string message;
+            Console.WriteLine("Enter Message:");
+            message=Console.ReadLine();
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+        }
+    }
+}
+
